9d2233bc1ae9f39d3b9136cee1371f3dac881033
Marcelo Rodas <marcelo.rodasbritez@unitn.it>
2018-04-30 17:35:31 +0200

    Improving the output.

--- newTagsExtractor.py
+++ newTagsExtractor.py
@@ -4,19 +4,7 @@ Implementation for new tags extractor (crowded) using a list of files with the s
 import operator
 import os
 
-# useless_words = ['a', 'an', 'of', 'in', 'the', 'and', 'on', 'for', 'with', 'to']
-
-
-def syn(word, lch_threshold=0.8):
-    for net1 in wn.synsets(word):
-        for net2 in wn.all_synsets():
-            try:
-                lch = net1.path_similarity(net2)
-            except:
-                continue
-            # The value to compare the LCH to was found empirically.
-            if lch is not None and lch >= lch_threshold:
-                yield (net1, net2, lch)
+useless_words = ['its', 'an', 'of', 'in', 'the', 'and', 'on', 'for', 'with', 'to']
 
 
 def readFile(filename):
@@ -33,25 +21,26 @@ def clean_wordList(a, b):
 
 for root, dirs, files in os.walk("resources/tagsOnly/", topdown=False):
     activity_tags = dict()
+    all_tags = dict()
     for fileName in files:
         myFile = readFile(fileName)
-        print('File: '+fileName)
+        # print('File: ' + fileName)
         for line in myFile:
 
             activity_words = line.split('\t')
-            #print(activity_words[0], "--")
+            # print(activity_words[0], "--")
             # similarities = set()
             freq = dict()
             activity = activity_words[0].lower()
             if (activity == '_id'):
                 continue
-            for i in range (3,6):
+            for i in range(3, 6):
                 word = activity_words[i].lower().strip()
-                freq[word]= 1;
+                freq[word] = 1;
             # similarities.add(word4);
-            #print(freq,"--")
+            # print(freq,"--")
             if (activity not in activity_tags):
-                activity_tags[activity]=freq
+                activity_tags[activity] = freq
             else:
                 freq2 = activity_tags[activity]
                 for i in range(3, 6):
@@ -60,13 +49,26 @@ for root, dirs, files in os.walk("resources/tagsOnly/", topdown=False):
                         freq2[word] = freq2[word] + 1
                     else:
                         freq2[word] = 1
+
+                    if (word in all_tags):
+                        all_tags[word] = all_tags[word] + 1
+                    else:
+                        all_tags[word] = 1
                 activity_tags[activity] = freq2
 
-    print("Results: ")
+    print("Results 01: Activities - Amount_Tags == Tags_List")
+    i = 0
+    size = 0
     for key, value in activity_tags.items():
         freq_sorted = sorted(value.items(), key=operator.itemgetter(1), reverse=True)
-        print (key,"==",freq_sorted)
+        i = i + 1
+        size = size + len(freq_sorted)
+        print(key, "-", len(freq_sorted), "== ", freq_sorted)
 
-    # freq_sorted = sorted(freq.items(), key=operator.itemgetter(1), reverse=True)
-    # similarities = freq_sorted[0:size]
-    # print("--", len(similarities), "--", similarities)
+    print("Average tags per activity:", size / i)
+
+    freq_sorted2 = sorted(all_tags.items(), key=operator.itemgetter(1), reverse=True)
+    print("Amount of unique tags:", len(freq_sorted2))
+    print("Results 02: Tag == frequency_used")
+    for key, value in freq_sorted2:
+        print(key, "==", value)
